5b3a86a634438e8e76896403c1737e3a2875dbe5
mhmtyilmaz <mylmz89@gmail.com>
2018-01-28 00:32:32 +0100

    Implement register for user

---
+++ src/main/java/com/mhmtyilmaz/controllers/UserController.java
@@ -0,0 +1,7 @@
+package com.mhmtyilmaz.controllers;
+
+/**
+ * Created by MEHMET on 27.1.2018.
+ */
+public class UserController {
+}
---
+++ src/main/java/com/mhmtyilmaz/pojos/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.mhmtyilmaz.pojos;
+
+/**
+ * Created by MEHMET on 27.1.2018.
+ */
+public class UserRegistration {
+}
